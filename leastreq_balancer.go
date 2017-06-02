576fe881dfaf6992bafc86f89952b2a9af373126
Ryota Arai <ryota.arai@gmail.com>
2017-06-02 15:14:16 +0900

    wip

--- leastreq_balancer.go
+++ leastreq_balancer.go
@@ -1,17 +1,52 @@
 package simproxy
 
+import (
+	"github.com/emirpasic/gods/sets/treeset"
+)
+
+type SetItem struct {
+	Requests int
+	Backend  *Backend
+}
+
 type LeastreqBalancer struct {
-	Backends []*Backend
+	set *treeset.Set
 }
 
 func NewLeastreqBalancer(backends []*Backend) *LeastreqBalancer {
-	return &LeastreqBalancer{
-		Backends: backends,
+	b := &LeastreqBalancer{}
+	b.set = treeset.NewWith(b.setComparator)
+
+	for _, backend := range backends {
+		b.AddBackend(backend)
 	}
+
+	return b
 }
 
 func (b *LeastreqBalancer) RetainServer() *Backend {
+	return nil
 }
 
 func (b *LeastreqBalancer) ReleaseServer(*Backend) {
+}
+
+func (b *LeastreqBalancer) AddBackend(backend *Backend) {
+	item := &SetItem{
+		Requests: 0,
+		Backend:  backend,
+	}
+	b.set.Add(item)
+}
+
+func (balancer *LeastreqBalancer) setComparator(a, b interface{}) int {
+	itemA := a.(*SetItem)
+	itemB := b.(*SetItem)
+
+	delta := itemA.Requests - itemB.Requests
+	if delta != 0 {
+		return delta
+	}
+
+	return itemA.Backend - itemB.Backend
 }
