7e1fb0b610b51853129d5d37238c9f52ded3c80d
Ryota Arai <ryota.arai@gmail.com>
2017-08-02 11:08:07 +0900

    Support more http/pprof endpoints.

--- cmd/simproxy/pprof.go
+++ cmd/simproxy/pprof.go
@@ -6,9 +6,16 @@ import (
 )
 
 func startPprofServer(addr string) {
+	m := http.NewServeMux()
+	m.Handle("/debug/pprof/", http.HandlerFunc(pprof.Index))
+	m.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
+	m.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
+	m.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
+	m.Handle("/debug/pprof/trace", http.HandlerFunc(pprof.Trace))
+
 	s := &http.Server{
 		Addr:    addr,
-		Handler: http.HandlerFunc(pprof.Index),
+		Handler: m,
 	}
 	go func() {
 		s.ListenAndServe()
